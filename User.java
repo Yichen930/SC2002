812cbcb72cf0e5e7acd229b6e1e64755088a3da8
XinYu1408 <tanx0156@e.ntu.edu.sg>
2025-10-27 17:44:19 +0800

    Add files via upload

---
+++ User.java
@@ -0,0 +1,27 @@
+public abstract class User {
+    private String id;
+    private String name;
+    private String password = "password";
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public boolean changePassword(String oldPassword, String newPassword){
+        
+        if (oldPassword.equals(password)){
+            password = newPassword;
+            return true;
+        }
+        else{
+            return false;
+        }
+    } 
+}
